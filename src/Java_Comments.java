7976a0fb70aa5c94848c09ee59d9eb7b1c1dc514
hashann99 <hashann99@gmail.com>
2022-12-07 16:24:17 +0530

    Java Comments

---
+++ src/Java_Comments.java
@@ -0,0 +1,33 @@
+// in this video we will see how to use comments in out program
+// to begin with, i will create a java class called java_comments
+class Java_Comments{
+    // in addition to, i will create main method here
+    public static void main(String args[]){
+
+        // there is a two ways to write comments in your java program
+
+        // 1. single-line comments
+
+        System.out.println("i'm working,no comments");
+        // System.out.println("i don't work,because i have a single line comments"); this line no work
+
+        // 2. multi-line comments
+
+        System.out.println("we are working, we don't have a comments");
+        System.out.println("we are working, we don't have a comments");
+        System.out.println("we are working, we don't have a comments");
+        System.out.println("we are working, we don't have a comments");
+
+        /*
+
+        System.out.println("we don't work,we have multi-line comments");
+        System.out.println("we don't work,we have multi-line comments");
+        System.out.println("we don't work,we have multi-line comments");
+        System.out.println("we don't work,we have multi-line comments");
+        System.out.println("we don't work,we have multi-line comments");
+
+         */
+
+    }
+
+}
